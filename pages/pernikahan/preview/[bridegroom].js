62e9c8dd92ea0eeaf60e65c98cda7694d5308ee0
Imanuel Pundoko <ilomon10@gmail.com>
2021-03-19 18:44:46 +0800

    add preview route to preview post

---
+++ pages/pernikahan/preview/[bridegroom].js
@@ -0,0 +1,59 @@
+import Head from "next/head";
+import { Client } from "../../../components";
+import Template from "../../../components/Template";
+
+const Preview = ({
+  slug,
+  post: {
+    id,
+    bride,
+    groom,
+    theme,
+    contract,
+    reception,
+    gallery,
+    music,
+  }
+}) => {
+  return (
+    <>
+      <Head>
+        <title>Undangan Pernikahan: {bride.nickname} dan {groom.nickname}</title>
+      </Head>
+      <Template
+        theme={theme}
+        post={{
+          id: id,
+          slug: slug
+        }}
+        bride={bride}
+        groom={groom}
+        contract={contract}
+        reception={reception}
+        gallery={gallery}
+        music={music}
+      />
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { bridegroom } = context.params;
+  let { data: post } = await Client.posts({
+    params: {
+      "_fields": "id,acf",
+      "slug": bridegroom
+    }
+  });
+  return {
+    props: {
+      slug: bridegroom,
+      post: {
+        id: post[0].id,
+        ...post[0].acf
+      }
+    }
+  }
+}
+
+export default Preview;
