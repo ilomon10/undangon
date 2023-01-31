5c7283e6af76351b543656ada7ff4f630cfdb8d7
Imanuel Pundoko <ilomon10@gmail.com>
2023-01-31 12:53:30 +0800

    Add more action to invitation list

--- components/manager/Invitations/List.js
+++ components/manager/Invitations/List.js
@@ -1,7 +1,14 @@
-import { Box } from "components/Grid";
+import { Box, Flex } from "components/Grid";
 import client from "components/client";
 import { useQuery } from "@tanstack/react-query";
-import { Button, Card, Menu, MenuDivider, MenuItem } from "@blueprintjs/core";
+import {
+  AnchorButton,
+  Button,
+  Card,
+  Menu,
+  MenuDivider,
+  MenuItem,
+} from "@blueprintjs/core";
 import Link from "next/link";
 
 export const InvitationList = () => {
@@ -14,7 +21,7 @@ export const InvitationList = () => {
           fields: {
             content: 0,
           },
-          populate: 1
+          populate: 1,
         });
       } catch (err) {
         console.error(err);
@@ -27,12 +34,57 @@ export const InvitationList = () => {
   if (!data) return null;
 
   return (
-    <Menu>
-      {data.map(({ _id, name, category }) => (
-        <Link key={_id} href={`/manager/invitation/editor/${_id}`} passHref>
-          <MenuItem text={name} label={category.name} />
-        </Link>
+    <Box>
+      {data.map(({ _id, name, category, slug }) => (
+        <Flex
+          py={2}
+          sx={{
+            borderBottom: "1px solid black",
+            borderBottomColor: "gray.3",
+            alignItems: "center",
+          }}
+        >
+          <Box flexGrow={1}>
+            <Link key={_id} href={`/manager/invitation/editor/${_id}`} passHref>
+              <Box as="a">{name}</Box>
+            </Link>
+          </Box>
+          <Box color={"gray.5"}>{category.name}</Box>
+          <Box pl={2}>
+            <Link key={_id} href={`/manager/invitation/editor/${_id}`} passHref>
+              <AnchorButton
+                small
+                title="Edit"
+                icon="edit"
+                minimal
+                target="_blank"
+              />
+            </Link>
+          </Box>
+          <Box pl={2}>
+            <Link key={_id} href={`/i/p/${slug}`} passHref>
+              <AnchorButton
+                small
+                title="Preview"
+                icon="share"
+                minimal
+                target="_blank"
+              />
+            </Link>
+          </Box>
+          <Box pl={2}>
+            <Link key={_id} href={`/i/share/${slug}`} passHref>
+              <AnchorButton
+                small
+                title="Share"
+                icon="send-message"
+                minimal
+                target="_blank"
+              />
+            </Link>
+          </Box>
+        </Flex>
       ))}
-    </Menu>
+    </Box>
   );
 };
