111c30d56fc1d48ab2f39efee80921e6328db603
Imanuel Pundoko <ilomon10@gmail.com>
2023-02-08 11:20:40 +0800

    First setup mantine ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,15 @@
 import "../styles/globals.scss";
-import "nprogress/nprogress.css";
 import "react-medium-image-zoom/dist/styles.css";
 
-import NProgress from "nprogress";
-import Router from "next/router";
 import config from "react-reveal/globals";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { HotkeysProvider } from "@blueprintjs/core";
+import { NavigationProgress } from "@mantine/nprogress";
+import { MantineProvider } from "@mantine/core";
+import { RouterTransition } from "components/RouterTransition";
 
 config({ ssrFadeout: true });
-
-Router.onRouteChangeStart = () => NProgress.start();
-Router.onRouteChangeComplete = () => NProgress.done();
-Router.onRouteChangeError = () => NProgress.done();
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,13 +21,16 @@ const queryClient = new QueryClient({
 
 function MyApp({ Component, pageProps }) {
   return (
-    <HotkeysProvider>
-      <UserProvider>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </UserProvider>
-    </HotkeysProvider>
+    <MantineProvider withNormalizeCSS>
+      <RouterTransition />
+      <HotkeysProvider>
+        <UserProvider>
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </UserProvider>
+      </HotkeysProvider>
+    </MantineProvider>
   );
 }
 
